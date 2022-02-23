d87861465d046b4a3657066f11a9403c68642b1b
conx42 <sangramjit1999roy@gmail.com>
2022-02-24 00:56:01 +0530

    vid_69 completed :)

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+//rafc or rafce -> for react arrow function component
+
+
+import Notes from './Notes';
+
+export const Home = () => {
+
+  return (
+    <div>
+      <Notes />
+      
+    </div>
+  )
+}
